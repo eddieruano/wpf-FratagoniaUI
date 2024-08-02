d2c4ca7aa681e93a586a2c8de93daed2d63f23fe
Edward Ruano <edward@ruanoestate.com>
2024-08-01 17:56:19 -0700

    -Pushing intial demo changes to then begin adding control projects via FratagoniaUI solution. Needed a demo proj  for handling.

---
+++ FratagoniaUI/FratagoniaUI.OSS/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using FratagoniaUI.OSS.Windows;
+
+namespace FratagoniaUI.OSS
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      /// Need to set to Windows fold OSS
+      MainWindow = new OSSMainWindow();
+      MainWindow.Show();
+      /// continue w/ base call
+      base.OnStartup(e);
+    }
+  }
+
+}
